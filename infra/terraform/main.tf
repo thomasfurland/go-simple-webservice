e280b4e57614432c29b3919092a3d5281c41d954
Thomas Furland <45860355+thomasfurland@users.noreply.github.com>
2025-09-19 17:34:50 +0900

    add simple terraform and k8s configs (#8)
    * add simple terraform and k8s configs
    
    * add infra action

---
+++ infra/terraform/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_container_cluster" "simple_web" {
+  name     = "simple-web"
+  location = var.region
+  enable_autopilot = true
+}
+
+resource "google_sql_database_instance" "postgres" {
+  name             = "simple-web-db"
+  database_version = "POSTGRES_15"
+  region           = var.region
+
+  settings {
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection = false
+}
+
+resource "google_sql_database" "default" {
+  name     = "appdb"
+  instance = google_sql_database_instance.postgres.name
+}
+
+resource "google_sql_user" "app" {
+  name     = "appuser"
+  instance = google_sql_database_instance.postgres.name
+  password = var.db_password
+}
